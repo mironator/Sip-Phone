2e0140ac7c394d107a69c9a85e8ee69867ef8378
Alexander Mironchenko <alexander.mironchenko@gmail.com>
2011-04-19 22:03:12 +0300

    second commit

--- SIPLib.cs
+++ SIPLib.cs
@@ -6,7 +6,8 @@ using System.Text;
 using System.Net;
 using System.Threading;
 using System.Net.Sockets;
-using System.Xml;
+
+using Microsoft.Win32;  //для чтения реестра (кодеки)
 
 //github.com    репозиторий
 //rfc4317       SDP
@@ -16,8 +17,25 @@ namespace SIPLib
 {
     public delegate void Del(string str);
     public delegate bool DelRequest(string str);
-    
 
+    public class CodecReader
+    {
+        public bool GetValue(string key)
+        {
+            object val = null;
+
+            RegistryKey currRegistryKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\ActiveMovie\\devenum\\{33D9A760-90C8-11D0-BD43-00A0C911CE86}");
+            if (currRegistryKey == null)    
+            {
+                return false;    //если ключ не открывается
+            }
+           
+            return true;
+        }
+
+
+
+    }
 
     public class Session    //сама сессия
     {
@@ -32,7 +50,6 @@ namespace SIPLib
         string SessionID;
         Thread WaitForAnswer;
 
-
         //==============конструктор==================
         public Session(System.Net.IPAddress myIP, int myPort, string ToIP, string ToUser, string FromUser, Del d, string ID)    //конструктор при звонке
         {
@@ -44,6 +61,7 @@ namespace SIPLib
             this.port = myPort;
             this.SessionID = ID;
             n++;
+            
         }
 
         //==============внешние функции==============
@@ -77,15 +95,6 @@ namespace SIPLib
             }
         }
 
-        /*public bool CheckSessionByFrom(string name,string ip)   //функция проверки по имени и IP 
-        {
-            if (this.ToUser == name )
-            {
-                return true;
-            }
-            return false;
-        }*/
-
         public bool CheckSessionByID(string ID)
         {
             if (this.SessionID == ID) return true;
@@ -95,13 +104,6 @@ namespace SIPLib
         public bool WatchInfo(string Info)
         {
             //DelOutput(Info);
-
-            if (Info.Contains("200 OK"))    //ответ
-            {
-                DelOutput("Получен ACK");
-                this.ACKDecompile();
-                return true;
-            }
 
             if (Info.Contains("BYE"))    //пока
             {
@@ -127,6 +129,47 @@ namespace SIPLib
                 return true;
             }
 
+            if (Info.Contains("SIP/2.0 1"))    //ответ    OK
+            {
+                DelOutput("Получено 1XX");
+                this._1XXDecompile(Info);
+                return true;
+            }
+
+            if (Info.Contains("SIP/2.0 2"))    //ответ    OK
+            {
+                DelOutput("Получено 2XX (согласие)");
+                this._2XXDecompile(Info);
+                return true;
+            }
+
+            if (Info.Contains("SIP/2.0 3"))    //ответ    OK
+            {
+                DelOutput("Получено 3XX");
+                this._3XXDecompile(Info);
+                return true;
+            }
+
+            /*if (Info.Contains("SIP/2.0 4"))    //ответ    OK
+            {
+                DelOutput("Получено 4XX");
+                this._4XXDecompile(Info);
+                return true;
+            }*/
+
+            if (Info.Contains("SIP/2.0 5"))    //ответ    OK
+            {
+                DelOutput("Получено 5XX");
+                this._5XXDecompile(Info);
+                return true;
+            }
+
+            if (Info.Contains("SIP/2.0 6"))    //ответ      DECLINE
+            {
+                DelOutput("Получен 6XX (отказ)");
+                this._6XXDecompile(Info);
+                return true;
+            }
 
             return false;
         }
@@ -147,7 +190,6 @@ namespace SIPLib
             //return false;
             
         }
-
         bool SendInfo(string Info)
         {
             System.Net.IPAddress ipAddress;         //IP того, кому посылаем
@@ -190,8 +232,6 @@ namespace SIPLib
 
             return true;
         }
-
-
                 //==создание и разбор внутринностей запросов==
         public void Invite()
         {
@@ -214,14 +254,12 @@ namespace SIPLib
             WaitForAnswer.Start();
 
         }
-
         string InviteDeCompile(string FromIP, string ToIP, string ToUser, string FromUser)
         {
             string Request = "";
             return Request;
         }
-
-        public void ACK()
+        /*public void ACK()
         {
             string Request = "";
             Request += "SIP/2.0 200 OK" + "\n";
@@ -237,9 +275,10 @@ namespace SIPLib
             DelOutput(Request);
             SessionConfirmed = true;
 
-        }
-        int ACKDecompile() { return 0; }
-        string BYECompile()
+        }*/
+        int OKDecompile(string Info) { return 0; }
+        
+        string BYE()
         {
             string Request = "";
             Request += "BYE sip:" + ToUser + " SIP/2.0 " + "\n";
@@ -247,13 +286,189 @@ namespace SIPLib
 
             return Request;
         }
-        int BYEDecompile() { return 0; }
-        string REGISTERCompile() { return null; }
-        int REGISTERDecompile() { return 0; }
+        int BYEDecompile(string Info) { return 0; }
+
+        string REGISTER() { return null; }
+        int REGISTERDecompile(string Info) { return 0; }
+                
+        
+        
+        
+        
+            //==создание и разбор внутринностей ответов==
+
+        public void _1XXCompile(string Info)
+        {
+        }
+        public void _1XXDecompile(string Info)
+        {
+        }
+
+        public void _2XXCompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+            string Request = "";
+            Request += "SIP/2.0 2" + _XX + " OK" + "\n";
+            Request += "From: " + this.MyName + " <sip:" + this.MyName + "@" + this.myIP.ToString() + ">" + "\n";
+            Request += "To: <sip: " + this.ToUser + "@" + this.ToIP + ">" + "\n";
+            Request += this.SessionID.ToString() + "\n";
+            Request += "Cseq: " + (++this.n).ToString() + " OK" + "\n";
+            Request += "Date: " + DateTime.Now.ToString() + "\n";
+            if (SDPRequired)
+                Request += "\n" + SDP();
+            SendInfo(Request);
+            DelOutput(Request);
+            if (EndSession)
+                this.CloseSession();
+        }
+        void _2XXDecompile(string Info)
+        {
+        }
+
+
+        public void _3XXCompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+            string Request = "";
+            Request += "SIP/2.0 3";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Multiple Choices\n"; break;
+                case "01": Request += _XX + " Moved Permanently\n"; break;
+                case "02": Request += _XX + " Moved Temporary\n"; break;
+                default: return;
+            }
+
+            Request += "From: " + this.MyName + " <sip:" + this.MyName + "@" + this.myIP.ToString() + ">" + "\n";
+            Request += "To: <sip: " + this.ToUser + "@" + this.ToIP + ">" + "\n";
+            Request += this.SessionID.ToString() + "\n";
+            Request += "Cseq: " + (++this.n).ToString();// " Decline" + "\n";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Multiple Choices\n"; break;
+                case "01": Request += _XX + " Moved Permanently\n"; break;
+                case "02": Request += _XX + " Moved Temporary\n"; break;
+                default: return;
+            }
+
+            Request += "Date: " + DateTime.Now.ToString() + "\n";
+            if (SDPRequired)
+                Request += "\n" + SDP();
+
+            SendInfo(Request);
+            DelOutput(Request);
+
+            if (EndSession)
+                this.CloseSession();
+        }
+        public void _3XXDecompile(string Info)
+        {
+        }
+
+        /*
+        public void _4XXCompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+        }
+        public void _4XXDecompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+        }
+        */
+
+        public void _5XXCompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+            string Request = "";
+            Request += "SIP/2.0 5";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Server Internal Error\n"; break;
+                case "01": Request += _XX + " Not Implemented\n"; break;
+                case "02": Request += _XX + " Bad Gateway\n"; break;
+                case "03": Request += _XX + " Service Unavailable\n"; break;
+                default: Request += "01 Not Implemented\n"; break;
+            }
+
+            Request += "From: " + this.MyName + " <sip:" + this.MyName + "@" + this.myIP.ToString() + ">" + "\n";
+            Request += "To: <sip: " + this.ToUser + "@" + this.ToIP + ">" + "\n";
+            Request += this.SessionID.ToString() + "\n";
+            Request += "Cseq: " + (++this.n).ToString();// " Decline" + "\n";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Server Internal Error\n"; break;
+                case "01": Request += _XX + " Not Implemented\n"; break;
+                case "02": Request += _XX + " Bad Gateway\n"; break;
+                case "03": Request += _XX + " Service Unavailable\n"; break;
+                default: Request += "01 Not Implemented\n"; break;
+            }
+
+            Request += "Date: " + DateTime.Now.ToString() + "\n";
+            if (SDPRequired)
+                Request += "\n" + SDP();
+
+            SendInfo(Request);
+            DelOutput(Request);
+
+            if (EndSession)
+                this.CloseSession();
+
+        }
+        public void _5XXDecompile(string Info)
+        {
+        }
+
+        public void _6XXCompile(string _XX, bool SDPRequired, bool EndSession)
+        {
+            string Request = "";
+            Request += "SIP/2.0 6";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Busy Everywhere\n"; break;
+                case "03": Request += _XX + " Decline\n"; break;
+                case "04": Request += _XX + " Does Not Exist Anywhere\n"; break;
+                case "06": Request += _XX + " Not Acceptable\n"; break;
+                default: Request += "03 Decline\n"; break;
+            }
+
+            Request += "From: " + this.MyName + " <sip:" + this.MyName + "@" + this.myIP.ToString() + ">" + "\n";
+            Request += "To: <sip: " + this.ToUser + "@" + this.ToIP + ">" + "\n";
+            Request += this.SessionID.ToString() + "\n";
+            Request += "Cseq: " + (++this.n).ToString();// " Decline" + "\n";
+            switch (_XX)
+            {
+                case "00": Request += _XX + " Busy Everywhere\n"; break;
+                case "03": Request += _XX + " Decline\n"; break;
+                case "04": Request += _XX + " Does Not Exist Anywhere\n"; break;
+                case "06": Request += _XX + " Not Acceptable\n"; break;
+                default: Request += "03 Decline\n"; break;
+            }
+
+            Request += "Date: " + DateTime.Now.ToString() + "\n";
+
+            if (SDPRequired)
+                Request += "\n" + SDP();
+
+            SendInfo(Request);
+            DelOutput(Request);
+
+            if (EndSession)
+                this.CloseSession();
+
+        }
+        public void _6XXDecompile(string Info)
+        {
+        }
 
         string SDP()    //<============================================= обязательно реализовать
         {
             return null;
+        }
+
+        string SDPcombine(string str)
+        {
+            string[] ms = str.Split('\n');
+            string Result = "";
+            foreach (string a in ms)
+            {
+                if (SDP().Contains(a)) Result += a + "\n";
+            }
+            return Result;
         }
     }
 
@@ -365,32 +580,33 @@ namespace SIPLib
             {
                 if (Info.Contains("INVITE "))   //при приходе инвайта
                 {
+                    tmp = Info.Remove(0, Info.IndexOf("To: <sip: ") + "To: <sip: ".Length);
+                    tmp = tmp.Remove(tmp.IndexOf('>'));
+                    tmp = tmp.Remove(0, tmp.IndexOf('@') + 1);
+                    // DelOutput("1"+tmp+"1"); //ToIp
+                    tmp2 = Info.Remove(0, Info.IndexOf("To: <sip: ") + "To: <sip: ".Length);
+                    tmp2 = tmp2.Remove(tmp2.IndexOf('>'));
+                    tmp2 = tmp2.Remove(tmp2.IndexOf('@'));
+                    //DelOutput("1" + tmp2 + "1"); //ToUser
+                    //DelOutput(From.Remove(From.IndexOf('@'))); //FromUser
+                    //DelOutput(From.Remove(0,From.IndexOf('@')+1));  //FromIp
+
+                    tmp3 = Info.Remove(0, Info.IndexOf("Call-ID"));
+                    tmp3 = tmp3.Remove(tmp3.IndexOf('\n'));
+
+
                     if (DelRequest1(From))  //спрашиваем об открытии новой сессии
                     {
-                        tmp = Info.Remove(0, Info.IndexOf("To: <sip: ") + "To: <sip: ".Length);
-                        tmp = tmp.Remove(tmp.IndexOf('>'));
-                        tmp = tmp.Remove(0, tmp.IndexOf('@') + 1);
-                       // DelOutput("1"+tmp+"1"); //ToIp
-                        tmp2 = Info.Remove(0, Info.IndexOf("To: <sip: ") + "To: <sip: ".Length);
-                        tmp2 = tmp2.Remove(tmp2.IndexOf('>'));
-                        tmp2 = tmp2.Remove(tmp2.IndexOf('@'));
-                        //DelOutput("1" + tmp2 + "1"); //ToUser
-                        //DelOutput(From.Remove(From.IndexOf('@'))); //FromUser
-                        //DelOutput(From.Remove(0,From.IndexOf('@')+1));  //FromIp
-
-                        tmp3 = Info.Remove(0, Info.IndexOf("Call-ID"));
-                        tmp3 = tmp3.Remove(tmp3.IndexOf('\n'));
+                        
 
                         Sessions.Add(new Session(System.Net.Dns.GetHostByName(System.Net.Dns.GetHostName()).AddressList[0], port, tmp, tmp2, From.Remove(From.IndexOf('@')), DelOutput, tmp3));
-                        Sessions.Last().ACK();  //вызов подтверждения
+                        Sessions.Last()._2XXCompile("00", true, false);  //вызов подтверждения
                     }
                     else
                     {
-                        return false;
+                        Sessions.Add(new Session(System.Net.Dns.GetHostByName(System.Net.Dns.GetHostName()).AddressList[0], port, tmp, tmp2, From.Remove(From.IndexOf('@')), DelOutput, tmp3));
+                        Sessions.Last()._6XXCompile("03", false, true);
                     }
-
-                    //послать утвердительный ответ из новосозданной сесии <=============================!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    //иначе отрицательный и забыть про всё
 
 
                 }
@@ -400,12 +616,12 @@ namespace SIPLib
                     tmp = tmp.Remove(tmp.IndexOf('\n'));
 
                     //DelOutput("ПРИШЁЛ ОТВЕТ");
+                    //DelOutput("Число сессий:" + Sessions.Count.ToString() + "\n");
 
-                    //DelOutput("Число сессий:" + Sessions.Count.ToString() + "\n" + "1" + From + "1");
                     foreach (Session s in Sessions)
                     {
                         //if (s.CheckSessionByFrom(tmp, From.Remove(0, From.IndexOf('@') + 1))) s.WatchInfo(Info);  //проверка по From (имя и IP) и запуск разбора запроса
-                        if (s.CheckSessionByID(tmp))
+                        if (s.CheckSessionByID(tmp))    //проверка по ID
                         {
                             s.WatchInfo(Info);
                             //DelOutput("Ответ дошёл до адресата: " + tmp);
